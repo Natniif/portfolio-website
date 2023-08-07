b9eb70315bfc8d86e4d26561d173310ea9e2e121
Fintan Hardy <fintan.hardy@gmail.com>
2023-08-07 15:26:15 +0100

    contact box + beginnng of portfolio section

--- js/index.js
+++ js/index.js
@@ -2,3 +2,32 @@ function scrollToTop() {
   const div = document.querySelector(".about-me");
   div.scrollIntoView({ behavior: "smooth" });
 }
+
+fetch("json/portfolio.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const output = document.querySelector(".portfolio");
+    data.project.forEach((project) => {
+      const out = addProjectsToPage(project);
+      output.innerHTML += out;
+    });
+  });
+
+function addProjects() {
+  // retrieve json and format to web page
+}
+
+document.querySelector(".form").addEventListener("submit", handleSubmit);
+function handleSubmit(e) {
+  const emailInput = document.querySelector("#email").value;
+  const messageInput = document.querySelector("#message").value;
+  console.log("button clicked");
+  if (emailInput.length != 0 && messageInput.length != 0) {
+    console.log("form submitted");
+  } else {
+    console.log("error");
+    e.preventDefault();
+  }
+}
